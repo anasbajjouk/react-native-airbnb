ff84fd948698131e41ce4683a599db5804845700
Anas Bajjouk <anasbajjouk@Anass-MacBook-Pro.local>
2021-01-10 16:45:33 +0100

    Adding homescreen

---
+++ src/screens/Home/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, Text, ImageBackground, Pressable } from 'react-native'
+import Fontisto from 'react-native-vector-icons/Fontisto'
+import styles from './styles'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Pressable
+        style={styles.searchButton}
+        onPress={() => console.info('search button is clicked')}
+      >
+
+        <Fontisto name={'search'} size={25} color={'#f15454'} />
+        <Text style={styles.searchButtonText}>Where are you going?</Text>
+      </Pressable>
+
+      <ImageBackground
+        source={require('../../../assets/images/wallpaper.jpg')}
+        style={styles.image}
+      >
+        <Text style={styles.title}>Go Near</Text>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => console.info('explore button is clicked')}
+        >
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 500,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title: {
+    padding: 25,
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: 'white',
+    width: '65%',
+  },
+  button: {
+    backgroundColor: '#fff',
+    marginLeft: 25,
+    marginTop: 25,
+    height: 40,
+    width: Dimensions.get('screen').width - 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: { fontSize: 16, fontWeight: 'bold' },
+  searchButton: {
+    backgroundColor: '#fff',
+    marginHorizontal: 10,
+    height: 60,
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 50,
+    zIndex: 100
+  },
+  searchButtonText: { fontSize: 16, fontWeight: 'bold' },
+})
+
+export default styles
