dcbd79ae859ed1c42b7d8242fa7083f17d679813
Anas Bajjouk <anasbajjouk@Anass-MacBook-Pro.local>
2021-01-23 18:06:12 +0100

    autocomplete

---
+++ src/screens/DestinationSearch/SuggestionRow.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Entypo from 'react-native-vector-icons/Entypo'
+import styles from './styles'
+
+const SuggestionRow = ({ item }) => {
+  return (
+    <View style={styles.row}>
+      <View style={styles.iconContainer}>
+        <Entypo name="location-pin" size={30} />
+      </View>
+      <Text>{item.description}</Text>
+    </View>
+  )
+}
+
+export default SuggestionRow
--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,36 +1,35 @@
 import { useNavigation } from '@react-navigation/native'
 import React, { useState } from 'react'
 import { View, Text, TextInput, FlatList, Pressable } from 'react-native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import Entypo from 'react-native-vector-icons/Entypo'
 import searchResults from '../../../assets/data/search'
 import styles from './styles'
+import SuggestionRow from './SuggestionRow'
 
 const DestinationSearchScreen = () => {
-  const [inputText, setInputText] = useState('')
+  // const [inputText, setInputText] = useState('')
   const navigation = useNavigation()
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        placeholder={'Where are you going?'}
-        value={inputText}
-        onChangeText={setInputText}
-      />
-
-      <FlatList
-        data={searchResults}
-        renderItem={({ item }) => (
-          <Pressable
-            style={styles.row}
-            onPress={() => navigation.navigate('Guests')}
-          >
-            <View style={styles.iconContainer}>
-              <Entypo name="location-pin" size={30} />
-            </View>
-            <Text>{item.description}</Text>
-          </Pressable>
-        )}
+      <GooglePlacesAutocomplete
+        placeholder="Where are you going?"
+        fetchDetails
+        suppressDefaultStyles
+        onPress={(data, details = null) => {
+          console.log(data, details)
+          navigation.navigate('Guests')
+        }}
+        query={{
+          key: process.env.REACT_APP_GOOGLE_API_KEY,
+          language: 'en',
+          types: '(cities)',
+        }}
+        styles={{
+          textInput: styles.textInput,
+        }}
+        renderRow={(item) => <SuggestionRow item={item} />}
       />
     </View>
   )
